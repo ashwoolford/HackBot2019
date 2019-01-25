07c0c6b0e069c5b7ea74cd27200a032ca7bd19c5
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2019-01-25 14:31:54 +0600

    first commit

--- cbot/wsgi.py
+++ cbot/wsgi.py
@@ -15,6 +15,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbot.settings")
 
 application = get_wsgi_application()
 
-
-from whitenose.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
